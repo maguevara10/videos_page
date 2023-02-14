679c8f67f5f53b01bd7a358c96d23c5a70739380
Chaps0218 <105067369+maguevara10@users.noreply.github.com>
2023-02-14 14:00:50 -0500

    Texto Basura de prueba

---
+++ client/src/pages/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+    return (
+        <div className="main">
+            <h1>
+                MAIN 1
+            </h1>
+            <h2>
+                MAIN 2
+            </h2>
+        </div>
+    );
+}
